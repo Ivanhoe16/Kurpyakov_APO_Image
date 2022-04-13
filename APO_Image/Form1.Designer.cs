0525b9666e179c25419feea95625d2af5335eed8
Ivanhoe16 <50368394+Ivanhoe16@users.noreply.github.com>
2022-04-14 00:14:05 +0300

    Initial commit

---
+++ APO_Image/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace APO_Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.getTextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(444, 236);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(453, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(189, 236);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(3, 6);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(444, 29);
+            this.loadImageButton.TabIndex = 2;
+            this.loadImageButton.Text = "Загрузить картинку";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click);
+            // 
+            // getTextButton
+            // 
+            this.getTextButton.Location = new System.Drawing.Point(453, 6);
+            this.getTextButton.Name = "getTextButton";
+            this.getTextButton.Size = new System.Drawing.Size(188, 29);
+            this.getTextButton.TabIndex = 3;
+            this.getTextButton.Text = "Распознать текст";
+            this.getTextButton.UseVisualStyleBackColor = true;
+            this.getTextButton.Click += new System.EventHandler(this.getTextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 281);
+            this.Controls.Add(this.getTextButton);
+            this.Controls.Add(this.loadImageButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.Button getTextButton;
+    }
+}
+
---
+++ APO_Image/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace APO_Image
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog loadImage = new OpenFileDialog();
+            loadImage.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (loadImage.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image = new Bitmap(loadImage.FileName);
+        }
+
+        private void getTextButton_Click(object sender, EventArgs e)
+        {
+            var ocr = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default);
+            var btm = (Bitmap)pictureBox1.Image;
+            Pix image = PixConverter.ToPix(btm);
+            var text = ocr.Process(image);
+            richTextBox1.Text = text.GetText();
+        }
+    }
+}
